939ab84d0cc336aeff81541d8ce1995599ebe1f4
manishSingh077 <manish.singh@dcmail.ca>
2025-01-22 13:32:54 -0500

    Added basic Express server setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, Ride Sharing App!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
